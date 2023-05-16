60eae33555ee3191fc6c886e263ba07265d8e112
yhes9604 <102063000+yhes9604@users.noreply.github.com>
2023-05-16 20:01:19 +0800

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./src/components/Home.vue"
+import Meal from "./src/components/Meal.vue"
+import MealItem from "./src/components/MealItem.vue"
+import Calculator from "./src/components/Calculator.vue"
+import Map from "./src/components/Map.vue"
+import MealSuper from "./src/components/MealSuper.vue"
+import MealShare from "./src/components/MealShare.vue"
+const routes = [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+        path: "/meal",
+        component: Meal,
+      }, 
+   
+      {
+        path: "/mealitem",
+        component: MealItem,
+      },
+      {
+        path: "/calculator",
+        component: Calculator,
+      },
+      {
+        path: "/map",
+        component: Map,
+      },
+      {
+        path:"/super",
+        component:MealSuper,
+      },
+      {
+        path:"/share",
+        component: MealShare,
+      }
+  ];
+  
+  const router = createRouter({
+    routes: routes,
+    history: createWebHistory(),
+  });
+  export { router };
+  
